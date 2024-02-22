1a3d2c3aa5f5537adb2584be6e14dc3dd2f49252
shauan juliano <133115943+x-xTORCHx-x@users.noreply.github.com>
2024-02-22 09:51:57 -0300

    Add files via upload

---
+++ string.js
@@ -0,0 +1,8 @@
+const texto1 = "ola mundo!";
+const texto2 = "ola mundo!";
+const senha = "senh1234";
+const stringnumbers = "777";
+
+const citacao = "meu nome e ";
+const myname = "shauan";
+console.log(citacao + myname)
