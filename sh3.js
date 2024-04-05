d946de124c8998913d7b645eb4ce0f2e37d4ba95
shauan juliano <133115943+x-xTORCHx-x@users.noreply.github.com>
2024-04-05 09:49:18 -0300

    Add files via upload

---
+++ sh3.js
@@ -0,0 +1,9 @@
+var idade = 20;
+
+function age(idade){
+    if (idade > 18){
+        console.log(`você é maior de idade, pois você tem ${idade} anos de idade`);
+    } else
+    console.log(`você é menor de idade, pois você tem ${idade} anos de idade`);
+};
+console.log(age(idade));
