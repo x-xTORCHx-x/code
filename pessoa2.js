5f1d1c3ea0edf5cf26db8384e9e9223a082acff2
shauan juliano <133115943+x-xTORCHx-x@users.noreply.github.com>
2024-05-09 12:29:20 -0300

    Create pessoaa.js

---
+++ pessoa2.js
@@ -0,0 +1,31 @@
+let pessoa = {
+    nome: 'John',
+    idade: 40,
+    profisao: 'General do Exercito Britanico'
+};
+
+function age(){
+    console.log(`${pessoa.nome}, ${pessoa.profisao}, tem ${pessoa.idade}, ano que vem ele faz ${(pessoa.idade + 1)}`)
+};
+
+function ag(){
+    pessoa.proflet pessoa = {
+    nome: 'John',
+    idade: 40,
+    profisao: 'General do Exercito Britanico'
+};
+
+function age(){
+    console.log(`${pessoa.nome}, ${pessoa.profisao}, tem ${pessoa.idade}, ano que vem ele faz ${(pessoa.idade + 1)}`)
+};
+
+function ag(){
+    pessoa.profisao = 'medico';
+    console.log(`${pessoa.nome}, ${pessoa.profisao}, tem ${pessoa.idade}, ano que vem ele faz ${(pessoa.idade + 1)}`)
+};
+console.log(age());
+console.log(ag());isao = 'medico';
+    console.log(`${pessoa.nome}, ${pessoa.profisao}, tem ${pessoa.idade}, ano que vem ele faz ${(pessoa.idade + 1)}`)
+};
+console.log(age());
+console.log(ag());
