5552379b504b9c2ec8e523f3ccced27dda00ac9a
shauan juliano <133115943+x-xTORCHx-x@users.noreply.github.com>
2024-06-25 09:54:07 -0300

    Add files via upload

--- sort.js
+++ sort.js
@@ -1,2 +1,23 @@
-var list = [4,2,5,6,1,3,7];
-console.log(list.sort((a,b) => a - b));
+function quicksort(arr){
+    if(arr.length <= 1){
+        return arr;
+    }
+let pivot = arr(Math.floor(arr.length/2));
+
+let left = [];
+let middle = [];
+let right = [];
+
+for(let element of arr){
+    if (element < pivot){
+        left.push(element);
+}else if (element === pivot){
+}else{
+    right.push(element);
+}
+}
+return quicksort(left).concat(middle).concat(quicksort(right));
+}
+function main(){
+    let arr = [3,6]
+}
